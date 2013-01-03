da289171a4ecf8b70734ed700d71716719815c66
nakada <nakada22@gmail.com>
2013-01-03 13:26:37 +0900

    月次のオプションメニュー追加（勤怠記録出力）

--- TimeCard/src/jp/co/timecard/MonthlyActivity.java
+++ TimeCard/src/jp/co/timecard/MonthlyActivity.java
@@ -21,9 +21,10 @@ import android.widget.TextView;
 
 /**
  * 月次画面。
+ * 
  * @author TomohiroTano
  */
- public class MonthlyActivity extends Activity implements View.OnClickListener {
+public class MonthlyActivity extends Activity implements View.OnClickListener {
 
 	private Calendar cal;
 	private int mYear;
@@ -37,7 +38,7 @@ import android.widget.TextView;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.monthly_main);
 
-		//表示する年月（最初は現在の年月）
+		// 表示する年月（最初は現在の年月）
 		cal = Calendar.getInstance();
 
 		// 初期表示月のセット
@@ -70,57 +71,75 @@ import android.widget.TextView;
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu){
+	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
 		getMenuInflater().inflate(R.menu.menu, menu);
+		
+		// Icon Set
+		menu.findItem(R.id.finish).setIcon(android.R.drawable.ic_menu_close_clear_cancel);
+		menu.findItem(R.id.return_top).setIcon(android.R.drawable.ic_menu_revert);
+		menu.findItem(R.id.rec_export).setIcon(android.R.drawable.ic_menu_save);
+
 		return true;
 	}
 
 	@Override
-	public boolean onPrepareOptionsMenu(Menu menu){
+	public boolean onPrepareOptionsMenu(Menu menu) {
 		super.onPrepareOptionsMenu(menu);
 		return true;
 	}
 
+	/*
+	 * メニューが選択された時の処理
+	 */
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item){
-		// メニュー
-		switch(item.getItemId()){
-		// トップ画面へ戻る
-		case R.id.return_top:
-			Intent i2 = new Intent();
-			 i2.setClassName(
-	                    "jp.co.timecard",
-	                    "jp.co.timecard.TopActivity");
-            startActivity(i2);
-            return true;
+	public boolean onOptionsItemSelected(MenuItem item) {
 
-		// 終了する
-		 case R.id.finish:
-			 AlertDialog.Builder dlg = new AlertDialog.Builder(this);
-			 dlg.setMessage("アプリを終了してよろしいですか？");
-			 dlg.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+		switch (item.getItemId()) {
+
+		case R.id.return_top:
+			// トップ画面へ戻る
+			Intent i2 = new Intent();
+			i2.setClassName("jp.co.timecard", "jp.co.timecard.TopActivity");
+			startActivity(i2);
+			return true;
+
+		case R.id.finish:
+			// 終了する
+			AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+			dlg.setMessage("アプリを終了してよろしいですか？");
+			dlg.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					//finish();
+					// finish();
 					// アクティビティをバックグラウンドに移動する
 					moveTaskToBack(true);
 				}
-			 });
-			 dlg.setNegativeButton("キャンセル", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-					}
-			 });
-			 dlg.create().show();
-			 return true;
-		 }
-		 return super.onOptionsItemSelected(item);
+			});
+			dlg.setNegativeButton("キャンセル",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+						}
+					});
+			dlg.create().show();
+			return true;
+			
+		case R.id.rec_export:
+			//TODO 勤怠記録出力
+			
+			return true;
+			
+		}
+	
+		return super.onOptionsItemSelected(item);
 	}
 
 	/**
 	 * 表示対象の年月を取得。
-	 * @param value 前月か次月ボタンの値
+	 * 
+	 * @param value
+	 *            前月か次月ボタンの値
 	 */
 	public void setTargetMonth(int value) {
 
@@ -130,7 +149,8 @@ import android.widget.TextView;
 		cal.set(mYear, mMonth, 1);
 
 		TextView tv = (TextView) findViewById(R.id.mon_target);
-		tv.setText(String.valueOf(mYear) + "年" + String.valueOf(mMonth+1) + "月");
+		tv.setText(String.valueOf(mYear) + "年" + String.valueOf(mMonth + 1)
+				+ "月");
 	}
 
 	/**
@@ -141,19 +161,20 @@ import android.widget.TextView;
 		final ArrayList<DailyState> dayOfMonth = new ArrayList<DailyState>();
 
 		Dao dao = new Dao(this);
-		
+
 		int dom = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-		for(int i = 1; i <= dom; i++) {
-			//DBから勤怠を取得してくる（なければ空欄）
+		for (int i = 1; i <= dom; i++) {
+			// DBから勤怠を取得してくる（なければ空欄）
 			DailyState ds = new DailyState();
-			
-			String crrent_mMonth = String.format("%1$02d",mMonth+1);
-			String disp_date = String.format("%1$02d", i)+"日";
+
+			String crrent_mMonth = String.format("%1$02d", mMonth + 1);
+			String disp_date = String.format("%1$02d", i) + "日";
 			ds.setDate(disp_date);
-			
-			String date_param = mYear + "/" + crrent_mMonth + "/" + String.format("%1$02d", i);
+
+			String date_param = mYear + "/" + crrent_mMonth + "/"
+					+ String.format("%1$02d", i);
 			ds.setTargetDate(date_param);
-			
+
 			// 戻り値より出勤時刻・退勤時刻を取得
 			String[] daily_param = dao.MonthlyList(date_param);
 			String attendance_time = daily_param[0];
@@ -161,31 +182,34 @@ import android.widget.TextView;
 			String break_time = daily_param[2];
 
 			SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-			try{
-				if (attendance_time != "" && leaveoffice_time != "" && break_time != ""){
+			try {
+				if (attendance_time != "" && leaveoffice_time != ""
+						&& break_time != "") {
 					Date at = sdf.parse(attendance_time);
 					Date lt = sdf.parse(leaveoffice_time);
 					Date bt = sdf.parse(break_time);
 					// 全ての時間があれば合計時間計算、セット
-					long sumtime = lt.getTime() - at.getTime()- bt.getTime()+1000*60*60*6;
+					long sumtime = lt.getTime() - at.getTime() - bt.getTime()
+							+ 1000 * 60 * 60 * 6;
 					ds.setWorkHour(sdf.format(sumtime));
 				}
 				ds.setAttendance(attendance_time);
 				ds.setLeave(leaveoffice_time);
 				ds.setBreakTime(break_time);
 				dayOfMonth.add(ds);
-			}catch(java.text.ParseException e){
+			} catch (java.text.ParseException e) {
 				e.printStackTrace();
 			}
 		}
 
 		MonthlyAdapter la = new MonthlyAdapter(getApplicationContext(),
-									android.R.layout.simple_list_item_1, dayOfMonth);
+				android.R.layout.simple_list_item_1, dayOfMonth);
 		ListView lv = (ListView) findViewById(R.id.listview);
 		lv.setAdapter(la);
 		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> adapterview, View view, int position, long id) {
+			public void onItemClick(AdapterView<?> adapterview, View view,
+					int position, long id) {
 				DailyState ds = dayOfMonth.get(position);
 				Intent i = new Intent(MonthlyActivity.this, DailyActivity.class);
 				i.putExtra("DailyState", ds);
