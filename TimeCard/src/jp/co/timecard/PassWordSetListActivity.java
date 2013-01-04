13a54f295d836c62a0fdc1932e4f575d71a62466
nakada <nakada22@gmail.com>
2013-01-04 20:12:13 +0900

    パスワード設定（編集）処理を実装

--- TimeCard/src/jp/co/timecard/PassWordSetListActivity.java
+++ TimeCard/src/jp/co/timecard/PassWordSetListActivity.java
@@ -1,40 +1,34 @@
 package jp.co.timecard;
 
-import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.HashMap;
 
-import jp.co.timecard.db.Dao;
 import jp.co.timecard.db.DbConstants;
 import jp.co.timecard.db.DbOpenHelper;
+import jp.co.timecard.db.TopDao;
 import android.app.Activity;
-import android.app.TimePickerDialog;
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.InputType;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class PassWordSetListActivity extends Activity {
 
 	ArrayList<HashMap<String, String>> mList;
-	BaseSetListAdapter adapter;
+	PassWordSetListAdapter adapter;
 	ListView listview;
 	View getView;
-	int mYear;
-	int mMonth;
-	int hourOfDay;
-	int minute;
-	boolean is24HourView;
-	DecimalFormat df = new DecimalFormat("00");
-	SimpleDateFormat timestamp_sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -52,119 +46,145 @@ public class PassWordSetListActivity extends Activity {
 	 * リスト表示
 	 */
 	private void ListViewDisp() {
-
-		// 時刻設定マスタよりデータ取得
-		Dao dao = new Dao(this);
-		String[] ini_time = dao.DailyDefaultTime();
+		
 		HashMap<String, String> item;
+		String[] listdata = {"月次リスト","基本時間設定","パスワード設定"};
 		
-		// 初期表示
-		item = new HashMap<String, String>();
-		item.put("title", "月次リスト");
-		item.put("desc", "現在の設定 ****");
-		mList.add(item);
-
-		item = new HashMap<String, String>();
-		item.put("title", "基本時間設定");
-		item.put("desc", "現在の設定 ****");
-		mList.add(item);
-
-		item = new HashMap<String, String>();
-		item.put("title", "パスワード設定");
-		item.put("desc", "現在の設定 ****");
-		mList.add(item);
+		for (int i = 0; i < listdata.length; i++) {
+			// 初期表示
+			item = new HashMap<String, String>();
+			item.put("title", listdata[i]);
+			item.put("desc", "現在の設定 ****");
+			mList.add(item);
+		}
 		
-		adapter = new BaseSetListAdapter(this, mList);
+		adapter = new PassWordSetListAdapter(this, mList);
 		listview.setAdapter(adapter);
 		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-					String currenttime = timestamp_sdf.format(Calendar
-						.getInstance().getTime());
 
-				// タイトル、設定
+				// タイトル取得
 				String title = mList.get(position).get("title");
+				TopDao td = new TopDao(getApplicationContext());
+				
 				//String desc = mList.get(position).get("desc");
 				//Log.d("debug", desc);
 				//Log.d("debug", String.valueOf(item.entrySet()));
-				ListViewClick(title, title, currenttime, position);
+				
+				if (title == "月次リスト") {
+					String password = td.PassWordGet("monthly");
+					ListViewClick(title, "monthly", password);
+				} else if (title == "基本時間設定") {
+					String password = td.PassWordGet("basetime");
+					ListViewClick(title, "basetime", password);
+				} else if (title == "パスワード設定") {
+					String password = td.PassWordGet("password");
+					ListViewClick(title, "password", password);
+				}
 			}
 		});
 	}
 
 	/*
-	 * 時刻設定リストクリック
+	 * パスワード設定リストクリック
 	 */
-	private void ListViewClick(String settitle, final String title,
-			final String update_date_time, final int position) {
-		TimePickerDialog timePickerDialog;
+	private void ListViewClick(final String title, final String screen_id, final String password) {
+		
 		final ContentValues cv = new ContentValues();
 		final DbOpenHelper helper = new DbOpenHelper(getApplicationContext());
 		final SQLiteDatabase db = helper.getWritableDatabase();
-
-		// 時刻設定マスタよりデータ取得
-		Dao dao = new Dao(this);
-		final String[] ini_time = dao.DailyDefaultTime();
-
-		// 始業時刻設定がクリックされた場合
-		if (title == "始業時刻設定") {
-			hourOfDay = Integer.parseInt(ini_time[0].substring(0, 2));
-			minute = Integer.parseInt(ini_time[0].substring(3, 5));
-		} else if (title == "終業時刻設定") {
-			hourOfDay = Integer.parseInt(ini_time[1].substring(0, 2));
-			minute = Integer.parseInt(ini_time[1].substring(3, 5));
-		} else if (title == "休憩時間設定") {
-			hourOfDay = Integer.parseInt(ini_time[2].substring(0, 2));
-			minute = Integer.parseInt(ini_time[2].substring(3, 5));
-		}
-		is24HourView = true;
-
-		TimePickerDialog.OnTimeSetListener TimeSetListener = new TimePickerDialog.OnTimeSetListener() {
-			public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-
-				// 初期表示時刻を設定、セット時更新処理
-				StringBuilder sb = new StringBuilder()
-						.append(df.format(hourOfDay)).append(":")
-						.append(df.format(minute));
-				String a = sb.toString();
-
-				ListView listView;
-			    listView = (ListView)findViewById(R.id.listview);
-
-				try {
-					if (title == "始業時刻設定") {
-						final View listDataView = listView.getChildAt(0);
-						TextView time_tv = (TextView) listDataView.findViewById(R.id.desc);
-						cv.put(DbConstants.COLUMN_START_TIME, a);
-						time_tv.setText("現在の設定 " + a); // 設定時刻画面反映
-						
-					} else if (title == "終業時刻設定") {
-						final View listDataView = listView.getChildAt(1);
-						TextView time_tv = (TextView) listDataView.findViewById(R.id.desc);
-						cv.put(DbConstants.COLUMN_END_TIME, a);
-						time_tv.setText("現在の設定 " + a); // 設定時刻画面反映
-						
-					} else if (title == "休憩時間設定") {
-						final View listDataView = listView.getChildAt(2);
-						TextView time_tv = (TextView) listDataView.findViewById(R.id.desc);
-						cv.put(DbConstants.COLUMN_BREAK_TIME, a);
-						time_tv.setText("現在の設定 " + sb); // 設定時刻画面反映
-					}
-
-					cv.put(DbConstants.COLUMN_UPDATE_DATETIME, update_date_time);
-					db.update(DbConstants.TABLE_NAME5, cv, null, null);
-				} finally {
-					db.close();
-				}
-			}
-		};
-
-		// 時刻設定ダイアログの作成
-		timePickerDialog = new TimePickerDialog(this, TimeSetListener,
-				hourOfDay, minute, is24HourView);
-		// timePickerDialog.setTitle("時間設定");
-		timePickerDialog.setMessage(settitle);
-		timePickerDialog.show();
+		final ListView listView;
+	    listView = (ListView)findViewById(R.id.listview);
+		
+	    // パスワード入力フィールドを二つ表示する(現在、変更後)
+ 		final EditText editView1 = new EditText(PassWordSetListActivity.this); // 現在
+ 		final EditText editView2 = new EditText(PassWordSetListActivity.this); // 変更後
+ 		final int FP = ViewGroup.LayoutParams.FILL_PARENT;
+     	editView1.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+ 		editView2.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+     	
+ 		LinearLayout layout = new LinearLayout(this);
+     	layout.setOrientation(LinearLayout.VERTICAL); // 垂直方向
+     	
+     	// レイアウトに２つのビューをセット
+     	TextView text1 = new TextView(this);
+        text1.setText("現在のパスワード");
+        layout.addView(text1, new LinearLayout.LayoutParams(FP, 50));
+        layout.addView(editView1, new LinearLayout.LayoutParams(FP, 50));
+     	
+     	TextView text2 = new TextView(this);
+        text2.setText("変更後のパスワード");
+        layout.addView(text2, new LinearLayout.LayoutParams(FP, 50));
+        layout.addView(editView2, new LinearLayout.LayoutParams(FP, 50));
+     	
+     	new AlertDialog.Builder(PassWordSetListActivity.this)
+         .setIcon(android.R.drawable.ic_dialog_info)
+         .setTitle("パスワードを入力してください。")
+         .setView(layout)
+         .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+             public void onClick(DialogInterface dialog, int whichButton) {
+            	 
+            	if (!editView1.getText().toString().equals(password)) {
+             		// 現在パスワード入力失敗時、Error
+             		Toast.makeText(PassWordSetListActivity.this, 
+                         "現在のパスワードを正しくを入力してください", 
+                         Toast.LENGTH_SHORT).show();
+             		return;
+             	} else {
+             		// 現在パスワード入力成功時
+             		if (editView2.getText().toString().length() == 0) {
+             			// 変更後のパスワード入力未入力時、Error
+             			Toast.makeText(PassWordSetListActivity.this, 
+                            "変更後のパスワードを入力してください", 
+                            Toast.LENGTH_SHORT).show();
+             			return;
+             		}
+             		
+             		Toast.makeText(PassWordSetListActivity.this, 
+						"パスワードを変更しました", 
+						Toast.LENGTH_SHORT).show();
+             		
+             		// DBへ変更後のパスワード反映
+            	    try {
+            	    	if (title == "月次リスト") {
+            				// 月次リストがクリックされた場合
+            				final View listDataView = listView.getChildAt(0);
+            				TextView time_tv = (TextView) listDataView.findViewById(R.id.desc);
+            				cv.put("password", editView2.getText().toString());
+            				time_tv.setText("現在の設定 " + "****"); // 月次リストパスワード画面反映
+            				db.update(DbConstants.TABLE_NAME7, cv, "screen_id=?", 
+            						new String[] {screen_id});
+            				
+            			} else if (title == "基本時間設定") {
+            				// 基本時間設定がクリックされた場合
+            				final View listDataView = listView.getChildAt(1);
+            				TextView time_tv = (TextView) listDataView.findViewById(R.id.desc);
+            				cv.put("password", editView2.getText().toString());
+            				time_tv.setText("現在の設定 " + "****"); // 基本時間設定パスワード画面反映
+            				db.update(DbConstants.TABLE_NAME7, cv, "screen_id=?", 
+            						new String[] {screen_id});
+            				
+            			} else if (title == "パスワード設定") {
+            				// パスワード設定がクリックされた場合
+            				final View listDataView = listView.getChildAt(2);
+            				TextView time_tv = (TextView) listDataView.findViewById(R.id.desc);
+            				cv.put("password", editView2.getText().toString());
+            				time_tv.setText("現在の設定 " + "****"); // パスワード設定パスワード画面反映
+            				db.update(DbConstants.TABLE_NAME7, cv, "screen_id=?", 
+            						new String[] {screen_id});
+            				
+            			 }
+            	    } finally {
+            			db.close();
+            		}
+             	}
+             }
+         })
+         .setNegativeButton("キャンセル", new DialogInterface.OnClickListener() {
+             public void onClick(DialogInterface dialog, int whichButton) {}
+         })
+         .show();
 	};
 }
